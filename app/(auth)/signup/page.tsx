2ce8f7919448cb5c3bc65a812d76705d6bff79bb
Martin Penkov <martin@penkov.me>
2024-06-04 01:23:55 +0300

    change field

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -47,7 +47,7 @@ export default function SignUp() {
                 <div className="w-full px-3">
                   <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="phone">Телефон <span
                       className="text-red-600">*</span></label>
-                  <input id="phone" type="email" className="form-input w-full text-gray-800"
+                  <input id="phone" type="text" className="form-input w-full text-gray-800"
                          placeholder="Въведете вашия телефон" required/>
                 </div>
               </div>
