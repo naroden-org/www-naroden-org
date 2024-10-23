92beda87035bef2a6864392e575e4c4a3f70601a
Martin Penkov <martin.penkov@eliseustech.com>
2024-10-24 00:15:57 +0300

    update pages

--- app/docs/privacy-policy/page.js
+++ app/docs/privacy-policy/page.js
@@ -4,6 +4,10 @@
 
 export default function PrivacyPolicy() {
   return (
-    <object data="/documents/privacy_policy.pdf" className="document" />
+    <object data='/documents/privacy_policy.pdf' className="document">
+        <p>Oops! Your browser doesn't support PDFs!</p>
+        <p><a href="/documents/privacy_policy.pdf">Download Instead</a></p>
+    </object>
+
   )
 }
